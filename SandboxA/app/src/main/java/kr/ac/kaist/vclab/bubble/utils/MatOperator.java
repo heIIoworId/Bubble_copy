fc0791f5fb4f265c736dc0a8697711ae258902ea
mineswwweeepr <ssmhan4@gmail.com>
2016-10-29 18:18:50 +0900

    Created activities package. Created models package. Implemented SoundMeter class.

--- SandboxA/app/src/main/java/kr/ac/kaist/vclab/bubble/utils/MatOperator.java
+++ SandboxA/app/src/main/java/kr/ac/kaist/vclab/bubble/utils/MatOperator.java
@@ -1,5 +1,9 @@
 package kr.ac.kaist.vclab.bubble.utils;
 
+import android.opengl.Matrix;
+
+import java.util.Arrays;
+
 /**
  * Created by 84395 on 10/29/2016.
  */
--- SandboxA/app/src/main/java/kr/ac/kaist/vclab/bubble/utils/VecOperator.java
+++ SandboxA/app/src/main/java/kr/ac/kaist/vclab/bubble/utils/VecOperator.java
@@ -1,8 +1,13 @@
 package kr.ac.kaist.vclab.bubble.utils;
 
+import android.opengl.Matrix;
+
+import java.util.Arrays;
+
 /**
  * Created by 84395 on 10/29/2016.
  */
 
 public class VecOperator {
+
 }
