45ffa36eb9fbd465e8ac229052459a905cb6feb1
Aavantgarde95 <95phm@kaist.ac.kr>
2016-11-02 10:21:35 +0900

    MapGen - Improved readability of PerlinNoise code

--- SandboxP/app/src/main/java/kr/ac/kaist/vclab/bubble/PerlinNoise.java
+++ SandboxP/app/src/main/java/kr/ac/kaist/vclab/bubble/PerlinNoise.java
@@ -7,24 +7,32 @@ package kr.ac.kaist.vclab.bubble;
 /* https://en.wikipedia.org/wiki/Perlin_noise */
 public class PerlinNoise {
     static public float noise(float x, float y, float z) {
-        int X = (int) Math.floor(x) & 255,                  // FIND UNIT CUBE THAT
-                Y = (int) Math.floor(y) & 255,                  // CONTAINS POINT.
-                Z = (int) Math.floor(z) & 255;
-        x -= Math.floor(x);                                // FIND RELATIVE X,Y,Z
-        y -= Math.floor(y);                                // OF POINT IN CUBE.
-        z -= Math.floor(z);
-        float u = fade(x),                                // COMPUTE FADE CURVES
-                v = fade(y),                                // FOR EACH OF X,Y,Z.
-                w = fade(z);
-        int A = p[X] + Y, AA = p[A] + Z, AB = p[A + 1] + Z,      // HASH COORDINATES OF
-                B = p[X + 1] + Y, BA = p[B] + Z, BB = p[B + 1] + Z;      // THE 8 CUBE CORNERS,
+        // find unit cube that contains point
+        int X = (int) Math.floor(x) & 255;
+        int Y = (int) Math.floor(y) & 255;
+        int Z = (int) Math.floor(z) & 255;
 
-        return lerp(w, lerp(v, lerp(u, grad(p[AA], x, y, z),  // AND ADD
-                grad(p[BA], x - 1, y, z)), // BLENDED
-                lerp(u, grad(p[AB], x, y - 1, z),  // RESULTS
-                        grad(p[BB], x - 1, y - 1, z))),// FROM  8
-                lerp(v, lerp(u, grad(p[AA + 1], x, y, z - 1),  // CORNERS
-                        grad(p[BA + 1], x - 1, y, z - 1)), // OF CUBE
+        // find relative x, y, z of point in cube
+        x -= Math.floor(x);
+        y -= Math.floor(y);
+        z -= Math.floor(z);
+
+        // compute face curves for each x, y, z
+        float u = fade(x);
+        float v = fade(y);
+        float w = fade(z);
+
+        // hash coordinates of the 8 cube corners...
+        int A = p[X] + Y, AA = p[A] + Z, AB = p[A + 1] + Z;
+        int B = p[X + 1] + Y, BA = p[B] + Z, BB = p[B + 1] + Z;
+
+        // and add blended results from 8 corners of cube
+        return lerp(w, lerp(v, lerp(u, grad(p[AA], x, y, z),
+                grad(p[BA], x - 1, y, z)),
+                lerp(u, grad(p[AB], x, y - 1, z),
+                        grad(p[BB], x - 1, y - 1, z))),
+                lerp(v, lerp(u, grad(p[AA + 1], x, y, z - 1),
+                        grad(p[BA + 1], x - 1, y, z - 1)),
                         lerp(u, grad(p[AB + 1], x, y - 1, z - 1),
                                 grad(p[BB + 1], x - 1, y - 1, z - 1))));
     }
@@ -38,9 +46,10 @@ public class PerlinNoise {
     }
 
     static float grad(int hash, float x, float y, float z) {
-        int h = hash & 15;                      // CONVERT LO 4 BITS OF HASH CODE
-        float u = h < 8 ? x : y,                 // INTO 12 GRADIENT DIRECTIONS.
-                v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+        // convert lo. 4 bits of hash code into 12 gradient directions
+        int h = hash & 15;
+        float u = h < 8 ? x : y;
+        float v = h < 4 ? y : h == 12 || h == 14 ? x : z;
         return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
     }
 
@@ -60,6 +69,8 @@ public class PerlinNoise {
     };
 
     static {
-        for (int i = 0; i < 256; i++) p[256 + i] = p[i] = permutation[i];
+        for (int i = 0; i < 256; i++) {
+            p[256 + i] = p[i] = permutation[i];
+        }
     }
 }
