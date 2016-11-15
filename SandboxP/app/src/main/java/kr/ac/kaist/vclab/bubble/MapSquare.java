890fad3b7b6dcddb1fb027a8faa349803c94c750
Avantgarde95 <95phm@kaist.ac.kr>
2016-11-16 05:21:46 +0900

    Added Blinn-phong shading (map-fshader2, map-vshader2)

--- SandboxP/app/src/main/java/kr/ac/kaist/vclab/bubble/MapSquare.java
+++ SandboxP/app/src/main/java/kr/ac/kaist/vclab/bubble/MapSquare.java
@@ -87,9 +87,9 @@ public class MapSquare {
 
         // prepare shaders and OpenGL program
         int vertexShader = MyGLRenderer.loadShaderFromFile(
-                GLES20.GL_VERTEX_SHADER, "map-vshader.glsl");
+                GLES20.GL_VERTEX_SHADER, "map-vshader2.glsl");
         int fragmentShader = MyGLRenderer.loadShaderFromFile(
-                GLES20.GL_FRAGMENT_SHADER, "map-fshader.glsl");
+                GLES20.GL_FRAGMENT_SHADER, "map-fshader2.glsl");
 
         mProgram = GLES20.glCreateProgram();             // create empty OpenGL Program
         GLES20.glAttachShader(mProgram, vertexShader);   // add the vertex shader to program
