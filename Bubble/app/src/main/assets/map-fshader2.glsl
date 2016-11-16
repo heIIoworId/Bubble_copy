8b7f0ecf1bea6d9f37bb425d54c00bed3141ed15
Avantgarde95 <95phm@kaist.ac.kr>
2016-11-17 01:42:01 +0900

    Integrated MapCube & SeaRectangle (other files are changed because of automatic reformatting...)

---
+++ Bubble/app/src/main/assets/map-fshader2.glsl
@@ -0,0 +1,46 @@
+precision mediump float;
+
+uniform vec3 uLight, uLight2, uColor;
+uniform sampler2D uTextureUnit;
+
+varying vec3 vNormal;
+varying vec3 vPosition;
+varying vec2 vTextureCoor;
+
+void main() {
+    vec3 tolight = normalize(uLight - vPosition);
+    vec3 tolight2 = normalize(uLight2 - vPosition);
+    vec3 normal = normalize(vNormal);
+
+    // diffuse
+    float diffuse = max(0.0, dot(normal, tolight));
+    diffuse += max(0.0, dot(normal, tolight2));
+    // vec3 intensity = uColor * diffuse;
+
+    // blinn-phong
+    float lambertian = max(dot(tolight, normal), 0.0);
+    float specular = 0.0;
+    vec3 specColor = vec3(1.0, 1.0, 1.0);
+
+    if (lambertian > 0.0) {
+        float shininess = 15.0;
+        vec3 viewDir = normalize(-vPosition);
+        vec3 halfDir = normalize(tolight + viewDir);
+        float specAngle = max(dot(halfDir, normal), 0.0);
+        specular = pow(specAngle, shininess);
+    }
+
+    // texture
+    vec4 textureColor = texture2D(uTextureUnit, vTextureCoor);
+
+    // color = texture + diffuse + blinn-phong
+    vec3 intensity = textureColor.xyz * diffuse * lambertian
+                        + specular * specColor;
+    // vec3 intensity = uColor * diffuse * lambertian + specular * specColor;
+
+    // haze
+    vec4 haze = vec4(0.7, 0.7, 0.7, 1.0);
+    float ratio = 1 + vPosition.z/33;
+
+    gl_FragColor = ratio * vec4(intensity, 1.0) + (1 - ratio) * haze;
+}
