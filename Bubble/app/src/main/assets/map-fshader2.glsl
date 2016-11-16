24767c9715573ecf197275ff66e25d3ac491d721
heIIoworId <jinjm37@gmail.com>
2016-11-17 03:53:04 +0900

    Add files via upload

--- Bubble/app/src/main/assets/map-fshader2.glsl
+++ Bubble/app/src/main/assets/map-fshader2.glsl
@@ -39,8 +39,8 @@ void main() {
     // vec3 intensity = uColor * diffuse * lambertian + specular * specColor;
 
     // haze
-    vec4 haze = vec4(0.7, 0.7, 0.7, 1.0);
-    float ratio = 1 + vPosition.z/33;
+    vec4 haze = vec4(0.5, 0.5, 0.5, 1.0);
+    float ratio = 1.0 + vPosition.z/37.0;
 
-    gl_FragColor = ratio * vec4(intensity, 1.0) + (1 - ratio) * haze;
+    gl_FragColor = ratio * vec4(intensity, 1.0) + (1.0 - ratio) * haze;
 }
