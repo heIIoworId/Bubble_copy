c9d84a0f49c60b8bccafd591f70ed706666a53b4
mineswwweeepr <ssmhan4@gmail.com>
2016-11-27 11:07:24 +0900

    minor changes

--- Bubble/app/src/main/java/kr/ac/kaist/vclab/bubble/models/BubbleCore.java
+++ Bubble/app/src/main/java/kr/ac/kaist/vclab/bubble/models/BubbleCore.java
@@ -54,54 +54,43 @@ public class BubbleCore extends Particle {
 
         // PREPARE SHADER AND GL PROGRAM
         int vertexShader = MyGLRenderer.loadShaderFromFile(
-                GLES20.GL_VERTEX_SHADER, "bubble-vshader.glsl");
+                GLES20.GL_VERTEX_SHADER, "bubblecore-vshader.glsl");
         int fragmentShader = MyGLRenderer.loadShaderFromFile(
-                GLES20.GL_FRAGMENT_SHADER, "bubble-fshader.glsl");
+                GLES20.GL_FRAGMENT_SHADER, "bubblecore-fshader.glsl");
 
-        mProgram = GLES20.glCreateProgram();             // create empty OpenGL Program
-        GLES20.glAttachShader(mProgram, vertexShader);   // add the vertex shader to program
-        GLES20.glAttachShader(mProgram, fragmentShader); // add the fragment shader to program
-        GLES20.glLinkProgram(mProgram);                  // create OpenGL program executables
+        // INITIATE PROGRAM, ATTACH SHADERS, AND LINK PROGRAM
+        mProgram = GLES20.glCreateProgram();
+        GLES20.glAttachShader(mProgram, vertexShader);
+        GLES20.glAttachShader(mProgram, fragmentShader);
+        GLES20.glLinkProgram(mProgram);
     }
 
+    // FIXME SG WORKING ON 161127
     public void draw(float[] projMatrix,
                      float[] modelViewMatrix,
-                     float[] modelMatrix,
-                     float[] viewMatrix,
                      float[] normalMatrix,
                      float[] light,
-                     float[] light2,
-                     float[] camera,
-                     int[] cubeTex) {
+                     float[] light2) {
 
         GLES20.glUseProgram(mProgram);
-        GLES20.glActiveTexture(GLES20.GL_TEXTURE3);
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_CUBE_MAP, cubeTex[0]);
 
-        // uniforms
+        // UNIFORM HANDLES
         mProjMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uProjMatrix");
         mModelViewMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uModelViewMatrix");
-        mModelMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uModelMatrix");
-        mViewMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uViewMatrix");
         mNormalMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uNormalMatrix");
         mColorHandle = GLES20.glGetUniformLocation(mProgram, "uColor");
         mLightHandle = GLES20.glGetUniformLocation(mProgram, "uLight");
         mLight2Handle = GLES20.glGetUniformLocation(mProgram, "uLight2");
-        mCameraHandle = GLES20.glGetUniformLocation(mProgram, "campos");
 
+        // SPECIFY THE VALUE OF UNIFORM HANDLES
         GLES20.glUniformMatrix4fv(mProjMatrixHandle, 1, false, projMatrix, 0);
         GLES20.glUniformMatrix4fv(mModelViewMatrixHandle, 1, false, modelViewMatrix, 0);
-        GLES20.glUniformMatrix4fv(mModelMatrixHandle, 1, false, modelMatrix, 0);
-        GLES20.glUniformMatrix4fv(mViewMatrixHandle, 1, false, viewMatrix, 0);
         GLES20.glUniformMatrix4fv(mNormalMatrixHandle, 1, false, normalMatrix, 0);
-
         GLES20.glUniform3fv(mColorHandle, 1, color, 0);
         GLES20.glUniform3fv(mLightHandle, 1, light, 0);
         GLES20.glUniform3fv(mLight2Handle, 1, light2, 0);
-        GLES20.glUniform3fv(mCameraHandle, 1, camera, 0);
-        GLES20.glUniform1i(mEnvHandle, 1);
 
-        // attributes
+        // ATTRIBUTES HANDLE
         mPositionHandle = GLES20.glGetAttribLocation(mProgram, "aPosition");
 
         GLES20.glEnableVertexAttribArray(mPositionHandle);
@@ -111,9 +100,10 @@ public class BubbleCore extends Particle {
                 GLES20.GL_FLOAT, false,
                 VERTEX_STRIDE, mVertexBuffer);
 
-        // DRAW
-        GLES20.glDrawArrays(GLES20.GL_LINE_STRIP, 0, trajectory.length / 3);
-        GLES20.glLineWidth(5.0f);
+        // Draw the cube
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, trajectory.length / 3);
+//        GLES20.glDrawArrays(GLES20.GL_LINE_LOOP, 0, vertices.length / 3);
+
         GLES20.glDisableVertexAttribArray(mPositionHandle);
     }
 
